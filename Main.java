64d701792adbbf694b291a8e342ab9632454506f
gabrielbarboza19 <gabrielbarboza21040512@outlook.es>
2024-08-13 14:17:28 -0300

    Cambie la clase Main

--- Main.java
+++ Main.java
@@ -4,19 +4,16 @@ public class Main {
         Persona persona1 = new Persona("Niko Bellick");
         Persona persona2 = new Persona("Roman Bellick");
         Persona persona3 = new Persona("Bruce Kibuttz");
-        Persona persona4 = new Persona("Malorie");
 
-        // Instanciar 3 objetos de la clase Tabla y asociar cada uno a una persona
+        // Instanciar 4 objetos de la clase Tabla y asociar cada uno a una persona
         Carton carton1 = new Carton(1, persona1);
         Carton carton2 = new Carton(2, persona2);
         Carton carton3 = new Carton(3, persona3);
-        Carton carton4 = new Carton(4, persona4);
         
         // Llenar las tablas con números aleatorios
         carton1.llenarNumeroCarton();
         carton2.llenarNumeroCarton();
         carton3.llenarNumeroCarton();
-        carton4.llenarNumeroCarton();
 
         // Crear objeto Bolita y llenar su lista de números salidos
         Bolillero bolillero = new Bolillero();
@@ -29,6 +26,5 @@ public class Main {
         bolillero.verificarNumeros(carton1);
         bolillero.verificarNumeros(carton2);
         bolillero.verificarNumeros(carton3);
-        bolillero.verificarNumeros(carton4);
     }
 }

ab63c36c69b3e9c60a09e4b8e44459e953b7fd43
gabrielbarboza19 <gabrielbarboza21040512@outlook.es>
2024-08-13 19:10:17 -0300

    Update Bolillero.java

--- Bolillero.java
+++ Bolillero.java
@@ -28,60 +28,8 @@ public class Bolillero {
         System.out.println("");
     }
 
-    public void verificarNumeros(Carton carton) {
-        ArrayList<Integer> numero_carton = carton.getNumeroCarton();
-        ArrayList<Integer> numeros_marcados = new ArrayList<>();
-
-        for (int num : numeros_salidos) {
-            if (numero_carton.contains(num)) {
-                numeros_marcados.add(num);
-            }
-        }
-
-        int cantidadNumeros = numero_carton.size();
-
-        if (!numeros_marcados.isEmpty()) {
-            System.out.println("   * TABLA N°" + carton.getIdentificador() + " (Jugador: " + carton.getPersonaQueJuega().getNombre() + "):");
-            System.out.println("");
-            System.out.println("       -Números de su tabla:   " + numero_carton);
-            System.out.println("       -Números que Marcó:     " + numeros_marcados + ".");
-            System.out.println("");
-        }
-
-        // Verifica si el jugador ganó la terna
-        if (numeros_marcados.size() >= 3) {
-            System.out.println("       -¡Felicidades " + carton.getPersonaQueJuega().getNombre() + "! ¡Ganaste la Terna!");
-        } else {
-            int faltanTerna = 3 - numeros_marcados.size();
-            System.out.println("       -Te faltan " + faltanTerna + " números para ganar la terna.");
-        }
-
-        // Verifica si el jugador ganó la quinta
-        if (numeros_marcados.size() >= 5) {
-            System.out.println("       -¡Felicidades " + carton.getPersonaQueJuega().getNombre() + "! ¡Ganaste la Quinta!");
-        } else if (numeros_marcados.size() < 5) {
-            int faltanQuinta = 5 - numeros_marcados.size();
-            System.out.println("       -Te faltan " + faltanQuinta + " números para ganar la quinta.");
-        }
-
-        // Verifica si le falta un número para ganar
-        if (numeros_marcados.size() == cantidadNumeros - 1) {
-            System.out.println("       -¡Estás a un número de ganar la lotería, " + carton.getPersonaQueJuega().getNombre() + "!");
-        }
-
-        System.out.println("");
-
-        // Verifica si el jugador ha ganado la lotería
-        if (numeros_marcados.size() == cantidadNumeros) {
-            System.out.println("   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-            System.out.println("");
-            System.out.println("                ¡Felicidades " + carton.getPersonaQueJuega().getNombre() + "!");
-            System.out.println("                ¡Ganaste la lotería!");
-            System.out.println("");
-            System.out.println("   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
-            System.out.println("");
-        }
-        System.out.println("------------------------------------------------------------------------------------------------------------");
-        System.out.println("");
+    // Método para obtener los números salidos
+    public ArrayList<Integer> getNumerosSalidos() {
+        return numeros_salidos;
     }
 }
